bf0aefc0972101d74daef998630d7136265ead1f
Jia <jovsofjdo@gmail.com>
2023-07-03 21:11:13 -0700

    Completed exercises 1-3

--- 1_VariablesExercise.js
+++ 1_VariablesExercise.js
@@ -10,23 +10,23 @@
 // But make sure you understand why the correct answer is right. ^&^&^&^&^&^&
 
 // Data Types and Variables
-
-// Exercise 1. Simply declare a variable named 'emptyVariable'
 let log = console.log
-const x = 'emptyVariable'
-log(x)
+// Exercise 1. Simply declare a variable named 'emptyVariable'
+const emptyVariable = ''
 // Exercise 2. Declare a variable called 'myName'
+var myName = ''
 
 // Exercise 3. Assign your name as the value for 'myName'
-
+myName = 'Jia'
+log(myName)
 // Exercise 4. Declare a variable called dreamDestination and give it the value
 // of what your dream vacation destination is.
+const dreamDestination = 'Barcelona'
+log(dreamDestination)
 
 // Exercise 5. Make the following variables and assignments
 // - a variable called num1 with a value greater than 1 and less than 10
 // - a variable called num2 with a value of greater than or equal to 1 and less
-// than or equal to 10
-
 // Exercise 6. Write a comment below explaining the difference between the rules
 // for num1 and num2 from Exercise 5. What numbers would be valid values for
 // num2 but not num1?
