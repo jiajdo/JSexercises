9401d910a10deb8420c2c90a3cd8c86b6b70a08c
Jia <jovsofjdo@gmail.com>
2023-07-12 13:30:25 -0700

    Completed object exercises

--- objexercises.js
+++ objexercises.js
@@ -1,60 +1,97 @@
 // Hello, object
 
-Write the code, one line for each action:
+// Write the code, one line for each action:
 
-Create an empty object user.
-Add the property name with the value John.
-Add the property surname with the value Smith.
-Change the value of the name to Pete.
-Remove the property name from the object.
+// Create an empty object user.
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+let user = {}
+user.name = "John"
+user.surname = "Smith"
+user.name = "Pete"
+delete user.name
 
 //Check for emptiness
 
-Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
 
-Should work like that:
+// Should work like that:
 
+// let schedule = {};
+
+// alert( isEmpty(schedule) ); // true
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) ); // false
+
+function isEmpty(obj){
+for (key in obj){
+ return false
+}
+return true
+}
 let schedule = {};
-
-alert( isEmpty(schedule) ); // true
+  
+console.log(isEmpty(schedule)); // true
 
 schedule["8:30"] = "get up";
 
-alert( isEmpty(schedule) ); // false
+console.log( isEmpty(schedule) ); // false
 
 //Sub object properties
-We have an object storing salaries of our team:
+// We have an object storing salaries of our team:
 
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+// If salaries is empty, then the result must be 0.
+
+function addSalaries(obj){
+  let sum = 0
+for (key in obj){
+   sum += obj[key] 
 }
-Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
-
-If salaries is empty, then the result must be 0.
+return sum
+}
+console.log(addSalaries({John: 100, Ann: 160, Pete: 130}))
 
 //Multiply numeric property values by 2
-Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+//Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
 
-For instance:
+function multiplyNumeric(obj){
+  for (key in obj){
+if (typeof obj[key] == "number"){
+    obj[key]*=2
+}
+  }
+}
 
-// before the call
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
-};
+// For instance:
 
-multiplyNumeric(menu);
+// // before the call
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
 
-// after the call
-menu = {
-  width: 400,
-  height: 600,
-  title: "My menu"
-};
-Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+// multiplyNumeric(menu);
 
-P.S. Use typeof to check for a number here.
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+// P.S. Use typeof to check for a number here.
 
