603c5976333bc72b59835f1cc9f5571ab513ec2a
Jia <jovsofjdo@gmail.com>
2023-07-14 06:53:43 -0700

    Completed objects challenges

--- 8_ObjectsChallenges.js
+++ 8_ObjectsChallenges.js
@@ -19,6 +19,17 @@
 // let obj3 = {};
 // keys(obj3); // []
 
+function keys(obj){
+    let arr = []
+    for (k in obj){
+arr.push(k)
+    }
+    return arr
+}
+console.log(keys({ a: 1, b: 2, c: 3 })) // ["a", "b", "c"]
+console.log(keys({ first: 'Matt', last: 'Lane' }))// ["first", "last"]
+console.log(keys({}))//[]
+
 // Exercise 2. Write a function called values, which accepts an object and returns an array of all of the values in the object.
 // IMPORTANT: Do not use the built in Object.values() function!
 // Examples:
@@ -29,6 +40,20 @@
 // let obj3 = {};
 // values(obj3); // []
 
+function values(obj){
+    let arr1 = []
+        for (k in obj){
+            arr1.push(obj[k])
+
+        }
+        return arr1
+    }
+
+console.log(values({ a: 1, b: 2, c: 3 })) //[1,2,3]
+console.log(values({ first: 'Matt', last: 'Lane', isDogOwner: true }))//["Matt", "Lane", true]
+console.log(values({}))// []
+
+//NOT ANSWERED
 // Exercise 3. Write a function called entries, which accepts an object and returns an array of arrays of key-value pairs.
 // In other words, each sub-array is an "entry" in the object with two elements: the first element is the key, and the second element is the value.
 // IMPORTANT: Do not use the built in Object.entries() function!
@@ -42,12 +67,65 @@
 // let obj3 = {};
 // entries(obj3); // []
 
+function entries(obj){
+    
+    arr3 = []
+    for (k in obj){
+        arr2 =[]
+        arr2.push(k)
+        arr2.push(obj[k])
+        arr3.push(arr2)
+    }
+  
+    return arr3
+    } 
+   
+
+
+console.log(entries({ a: 1, b: 2, c: 3 })) // [["a",1], ["b",2], ["c",3]]
+console.log(entries({ first: 'Matt', last: 'Lane', isDogOwner: true })) 
+//[["first","Matt"], ["last","Lane"], ["isDogOwner",true]]
+console.log(entries({})) //[]
+
 // Exercise 4. Write a function called pluck, which takes an array of objects and the name of a key.
 // The function should return an array containing the value associated with that key for each object, or undefined if that key is not present in the object.
 // Examples:
 // pluck([{ name: "Tim" }, { name: "Matt" }, { name: "Elie" }],'name')
 // ["Tim", "Matt", "Elie"]
 // pluck([{ name: "Tim", isBoatOwner: true }, { name: "Matt", isBoatOwner: false }, { name: "Elie" }],'isBoatOwner')
+
+// function pluck(arr, k){
+//     let x = []
+//     let count = 0
+//     for (i in arr){
+//         if (i >= count && Object.hasOwn(arr[i], k) === false){
+//             x.push(undefined)
+//         }
+//         if(i >= count && Object.hasOwn(arr[i], k) === true){
+//             x.push(Object.values(arr[i]))
+//             count++    
+//         }
+//     }
+//         return x.flat()
+//     }
+    
+
+
+function pluck(arr, k){
+let x = []
+for (i in arr){
+    if (arr[i].hasOwnProperty(k)){
+        x.push(arr[i][k])
+    }
+    else x.push(undefined)
+    }
+        return x  
+    }
+  
+
+console.log(pluck([{ name: "Tim" }, { name: "Matt" }, { name: "Elie" }],'name'))
+// ["Tim", "Matt", "Elie"]
+console.log(pluck([{ name: "Tim", isBoatOwner: true }, { name: "Matt", isBoatOwner: false }, { name: "Elie" }],'isBoatOwner'))
 // [true, false, undefined]
 
 // Exercise 5. Write a function called stringFromObject that generates a string from an object's key/value pairs.
@@ -60,10 +138,33 @@
 // "name = Elie, job = Instructor, isCatOwner = false"
 // stringFromObject({}); // ""
 
+function stringFromObject(obj){
+ return Object.entries(obj).map(([key, value]) => `${key} = ${value}`).join(', ')
+ 
+}
+
+console.log(stringFromObject({ a: 1, b: '2' })) // "a = 1, b = 2"
+console.log(stringFromObject({ name: 'Elie', job: 'Instructor', isCatOwner: false })) 
+//"name = Elie, job = Instructor, isCatOwner = false"
+
+
 // Exercise 6. Write a function called minMaxKeyInObject that accepts an object with numeric keys.*
 // The function should return an array with the following format: [lowestKey, highestKey]
 // Examples:
 // minMaxKeyInObject({ 2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e' });
 // [1, 10]
 // minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' });
-// [1, 4]
+//[1, 4]
+
+function minMaxKeyInObject(obj){
+let keyArr = Object.keys(obj)
+let newArr = []
+newArr.push(keyArr[0], keyArr[keyArr.length-1])
+return newArr
+
+}
+
+console.log(minMaxKeyInObject({ 2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e' }))
+// [1, 10]
+console.log(minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' }))
+//[1, 4]
